98cc1a300500f90038a28ede71ddaf61fccb4a14
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-13 20:41:47 -0300

    feat: updates tasks.service and tasks.controller in src/tasks/

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { type Task, TasksService } from './tasks.service';
 
-@Controller({})
-export class TasksController {}
+@Controller('/tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(@Query() query: any) {
+    console.log(query);
+    return this.tasksService.getTasks();
+  }
+
+  @Get(':id')
+  getTask(@Param('id') id: string) {
+    return this.tasksService.getTask(+id);
+  }
+
+  @Post()
+  createTask(@Body() task: Task) {
+    console.log(task);
+    return this.tasksService.createTask(task);
+  }
+
+  @Put()
+  updateTask() {
+    return this.tasksService.updateTask();
+  }
+
+  @Delete()
+  deleteTask() {
+    return this.tasksService.deleteTask();
+  }
+
+  @Patch()
+  updateTaskStatus() {
+    return this.tasksService.updateTaskStatus();
+  }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,45 @@
 import { Injectable } from '@nestjs/common';
 
+export interface Task {
+  id: number;
+  title: string;
+  description: string;
+  status: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getTasks() {
+    return this.tasks;
+  }
+
+  getTask(id: number) {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  createTask(task: Task) {
+    console.log(task);
+    this.tasks.push({
+      ...task,
+      id: this.tasks.length + 1,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+    return task;
+  }
+
+  updateTask() {
+    return 'Task updated successfully';
+  }
+
+  deleteTask() {
+    return 'Task deleted successfully';
+  }
+
+  updateTaskStatus() {
+    return 'Task status updated successfully';
+  }
+}
