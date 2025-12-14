d9710fdf2e59fd0d55ff9078144800e88f2c7a39
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-14 15:06:45 -0300

    feat: adds Req and Res from express to src/hello/hello.controller.ts

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
+import type { Request, Response } from 'express';
 
 @Controller()
 export class HelloController {
   @Get('/')
-  index() {
-    return 'Home page';
+  index(@Req() request: Request, @Res() response: Response) {
+    response.status(200).json({ message: 'Hello World! 👋' });
   }
 }
