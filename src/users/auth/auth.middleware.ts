04485c9a6516185107481c2d3d4098a091ac9937
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-15 14:57:22 -0300

    feat: adds auth middleware files to src/users/auth/

---
+++ src/users/auth/auth.middleware.ts
@@ -0,0 +1,19 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: () => void) {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    if (authorization !== 'Bearer 1234') {
+      return res.status(401).json({ message: 'Unauthorized token' });
+    }
+
+    next();
+  }
+}
