5a8e349010df5f253c955f0a2466bdee9fb2d1e8
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-15 13:46:05 -0300

    feat: adds validate-user.pipe files to src/hello/pipes/validate-user/

---
+++ src/hello/pipes/validate-user/validate-user.pipe.ts
@@ -0,0 +1,23 @@
+import {
+  ArgumentMetadata,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+
+@Injectable()
+export class ValidateUserPipe implements PipeTransform {
+  transform(value: { name: string; age: string }, metadata: ArgumentMetadata) {
+    console.log('value', value);
+    console.log('metadata', metadata);
+
+    const ageNumber = parseInt(value.age.toString(), 10);
+
+    if (isNaN(ageNumber)) {
+      throw new HttpException('Age must be a number', HttpStatus.BAD_REQUEST);
+    }
+
+    return { ...value, age: ageNumber };
+  }
+}
