1f56243f9a237e5070abaffebd9bfedd3820edde
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-15 16:59:49 -0300

    feat: adds and implements ApiOperation and ApiResponse in functions in src/tasks/tasks.controller.ts

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -11,23 +11,29 @@ import {
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto, UpdateTaskDto } from './dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('/tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get all tasks' })
+  @ApiResponse({ status: 200, description: 'Returns all tasks' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
   getAllTasks(@Query() query: any) {
     console.log(query);
     return this.tasksService.getTasks();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get task by id' })
   getTask(@Param('id') id: string) {
     return this.tasksService.getTask(parseInt(id));
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create task' })
   createTask(@Body() task: CreateTaskDto) {
     return this.tasksService.createTask(task);
   }
