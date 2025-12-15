dda43f390251855a2baa3e4a1fdb67fa62f71323
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-15 14:59:58 -0300

    feat: adds prisma to src/users/users.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,64 +1,23 @@
 import { Injectable } from '@nestjs/common';
-
-export interface User {
-  id: number;
-  email: string;
-  password: string;
-  firstName: string;
-  lastName: string;
-  emailVerified: boolean;
-  birthDate: Date;
-  role: string;
-  phone: string;
-  createdAt: Date;
-  updatedAt: Date;
-}
+import { CreateUserDto } from './dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class UsersService {
-  private users: User[] = [
-    {
-      id: 1,
-      email: 'admin@example.com',
-      password: 'password1',
-      firstName: 'Admin',
-      lastName: 'Person',
-      emailVerified: true,
-      birthDate: new Date('1990-01-01'),
-      role: 'admin',
-      phone: '123-456-7890',
-      createdAt: new Date('2022-01-01'),
-      updatedAt: new Date('2022-01-01'),
-    },
-    {
-      id: 2,
-      email: 'user@example.com',
-      password: 'password1',
-      firstName: 'User',
-      lastName: 'User',
-      emailVerified: true,
-      birthDate: new Date('1990-01-01'),
-      role: 'user',
-      phone: '123-456-7890',
-      createdAt: new Date('2022-01-01'),
-      updatedAt: new Date('2022-01-01'),
-    },
-    {
-      id: 3,
-      email: 'user2@example.com',
-      password: 'password1',
-      firstName: 'User',
-      lastName: 'User',
-      emailVerified: true,
-      birthDate: new Date('1990-01-01'),
-      role: 'user',
-      phone: '123-456-7890',
-      createdAt: new Date('2022-01-01'),
-      updatedAt: new Date('2022-01-01'),
-    },
-  ];
+  constructor(private prisma: PrismaService) {}
 
-  getUsers(): User[] {
-    return this.users;
+  getUsers() {
+    return this.prisma.user.findMany();
+  }
+
+  createUser(user: CreateUserDto) {
+    return this.prisma.user.create({
+      data: {
+        ...user,
+        role: 'customer',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    });
   }
 }
