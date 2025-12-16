1efc1dae799524aab17a11a5c352578dd8d26dd1
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-16 08:19:37 -0300

    feat: adds and implements ApiProperty in src/tasks/dto/create-task.dto.ts

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,16 +1,20 @@
 import { IsBoolean, IsString, MaxLength, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTaskDto {
+  @ApiProperty()
   @IsString()
   @MinLength(3)
   @MaxLength(50)
   title: string;
 
+  @ApiProperty()
   @IsString()
   @MinLength(10)
   @MaxLength(255)
   description: string;
 
+  @ApiProperty()
   @IsBoolean()
   status: boolean;
 }
